30d0c9b9388a5a55a25f8d94deb664fa1905f6a4
Erik Svensson <eosvensson@gmail.com>
2020-07-07 09:55:48 +0200

    XML Constructor Injection

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -1,5 +1,8 @@
 package se.lexicon;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import se.lexicon.data.AppUserDAO;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,13 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        AppUserDAO appUserDAO = context.getBean(AppUserDAO.class);
+
+        System.out.println(appUserDAO.createNewAppUser("test@test.com", "1234", "Testy"));
+
+        context.close();
     }
 }
